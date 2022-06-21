6e6cf9bd85ebebd73f834d462566d4a863f39dee
Nurkholiq <mnurkholiq114@gmail.com>
2022-06-21 11:08:12 +0700

    bug

--- index.js
+++ index.js
@@ -21,18 +21,18 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
   },
 });
 
 require("./src/socket")(io);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use("/api/v1/", router);
 
 app.get("/", (req, res) => {
-  res.send("Hello Developer!!");
+  res.send("Hello Developer!!!");
 });
 
 server.listen(port, () => {
